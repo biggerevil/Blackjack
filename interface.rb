1112e982ec80949e9fb2f98d0fcbceea4747ba45
Stepan <tugunrus@icloud.com>
2020-12-24 11:43:33 +0300

    Перенёс вывод из game.rb в interface.rb

--- interface.rb
+++ interface.rb
@@ -13,10 +13,11 @@ class Interface
     loop do
       @game.start_game
 
-      @game.player_turn
+      player_turn
       @game.dealer_turn
 
-      @game.end_game
+      player_score, dealer_score = @game.end_game
+      show_winner(player_score, dealer_score)
 
       puts 'Желаете играть дальше? (y/n)'
       answer = gets.chomp
@@ -27,5 +28,59 @@ class Interface
   def ask_name
     print 'Введите ваше имя: '
     gets.chomp
+  end
+
+  def player_turn
+    puts 'Ваши карты:'
+    @game.player.print_cards
+
+    puts 'Ваше действие?'
+    puts '1 - Пропустить ход'
+    puts '2 - Взять карту'
+    puts '3 - Открыть карты'
+    action = gets.chomp.to_i
+    make_action(action)
+  end
+
+  def show_winner(player_score, dealer_score)
+    puts 'Ваши карты:'
+    @game.player.print_cards
+
+    puts 'Карты дилера:'
+    @game.dealer.print_cards
+
+    if player_score == dealer_score || (dealer_score > 21 && player_score > 21)
+      puts "Ничья! Игрок = #{player_score}, дилер = #{dealer_score}"
+      @game.player.get_money_back
+      @game.dealer.get_money_back
+      return
+    end
+
+    if player_score < 22 && (player_score > dealer_score || dealer_score > 21)
+      puts "Победил игрок со счётом #{player_score}!"
+      @game.player.add_win_to_bank
+      return
+    end
+
+    if dealer_score < 22 && (dealer_score > player_score || player_score > 21)
+      puts "Победил дилер со счётом #{dealer_score}!"
+      @game.dealer.add_win_to_bank
+      nil
+    end
+  end
+
+  def make_action(action)
+    case action
+    when 1
+      puts 'Вы выбрали пропустить ход'
+    when 2
+      puts 'Вы выбрали взять ещё одну карту'
+      @game.player_take_card
+      @game.print_statuses
+    when 3
+      puts 'Вы выбрали открыть карты'
+    else
+      raise 'Неизвестное действие'
+    end
   end
 end
